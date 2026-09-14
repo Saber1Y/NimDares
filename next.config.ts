fd2cf1a829f6fdad2ecfeddf5be39bb08b2d2f91
Saber1Y <ayodejiakintobi1@gmail.com>
2026-09-14 17:46:14 +0100

    dev: allow current LAN origin for Next resources

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ["@nimiq/core", "@prisma/client", "@prisma/adapter-pg"],
-  allowedDevOrigins: ["192.168.1.145"],
+  allowedDevOrigins: ["192.168.1.137"],
 };
 
 export default nextConfig;
